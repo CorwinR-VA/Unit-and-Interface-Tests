ed97383c99ce5cb71dff80f0598e2116e837cf0f
Corwin Rainier <corwinrainier89@gmail.com>
2022-07-04 17:14:30 -0300

    Initial Commit

---
+++ Unit and Interface Tests/Unit_and_Interface_TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unit_and_Interface_TestsApp.swift
+//  Unit and Interface Tests
+//
+//  Created by Corwin Rainier on 7/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Unit_and_Interface_TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
